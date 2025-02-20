129c257b5f24f772de59be114f3a4d7193dac20b
lucia-sebe <luli.se04@gmail.com>
2025-02-20 17:07:31 -0300

    v10

--- script.js
+++ script.js
@@ -10,11 +10,51 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const navLinks = document.querySelectorAll('.nav-link');
 
-  const actualUrl = window.location.href;
+  const actualUrl = window.location.pathname;
+
 
   navLinks.forEach(link => {
-      if (link.href === actualUrl) {
+      if (link.pathname === actualUrl || (link.pathname === '/index.html' && actualUrl === '/')) {
           link.classList.add('activado');
       }
+  });
+
+  const imageAssets = [
+    './imagenes/foto1.webp',
+    './imagenes/foto2.webp',
+    './imagenes/foto3.webp',
+    './imagenes/foto4.webp',
+    './imagenes/catalogo.webp',
+    './imagenes/linkedin.svg',
+    './imagenes/behance.svg',
+    './imagenes/gmail.svg',
+    './imagenes/rotar_celular.png',
+    './imagenes/vans_afiche1.webp',
+    './imagenes/vans_afiche1_mu.webp',
+    './imagenes/vans_afiche2.webp',
+    './imagenes/vans_afiche2_mu.webp',
+    './imagenes/vans_afiche3.webp',
+    './imagenes/vans_afiche3_mu.webp',
+    './imagenes/pizza-top.svg',
+    './imagenes/pizza-top-original.png',
+    './videos/el_cuento_uruguayo.gif',
+    './videos/dictadura_del_diseno.gif',
+    './videos/totem_cervezas.gif',
+    './videos/en_bici.gif'
+  ];
+
+  imageAssets.forEach(src => {
+    const img = new Image();
+    img.src = src;
+  });
+
+  const videoAssets = [
+    './videos/plantas.mp4'
+  ];
+
+  videoAssets.forEach(src => {
+    const video = document.createElement('video');
+    video.src = src;
+    video.preload = 'auto';
   });
 });
